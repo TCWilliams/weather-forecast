bfed6f3a4270756a7ce814f57b2866cd74c9b0ac
Tim Williams <tim@The-Computer.local>
2019-05-03 13:45:35 +1200

    resize map

--- client/src/components/Map.js
+++ client/src/components/Map.js
@@ -7,7 +7,7 @@ import { setLocation } from '../actions'
 
 const MapComponent = compose(
   withProps({
-    googleMapURL: 'https://maps.googleapis.com/maps/api/js?key=AIzaSyABFOtQL9xThk0kB7-TnphG5aatlzIvLX4&v=3.exp&libraries=geometry,drawing,places',
+    googleMapURL: 'https://maps.googleapis.com/maps/api/js?key=[SECRET]&v=3.exp&libraries=geometry,drawing,places',
     loadingElement: <div style={{ height: '100%' }} />,
     containerElement: <div style={{ height: '25em', width: '100%' }} />,
     mapElement: <div style={{ height: '100%' }} />,
